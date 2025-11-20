86ceb2f91d534ca9e559eec0ff506635e1b1fb4c
zaid-ansari-dev <zaid.ansari.dev@proton.me>
2025-11-21 04:13:37 +0530

    import math module

---
+++ Basics/one.py
@@ -0,0 +1,27 @@
+import math
+
+# rounding
+print(round(3.8))
+print(abs(-2))
+
+print(math.ceil(2.2))
+
+
+# taking inputs
+x = input("x: ")
+print(type(x))
+y = int(x) + 1
+print(f"x: {x}, y: {y}")
+# type conversion
+int(x)
+# float(x)
+# bool(x)
+# str(x)
+
+
+# falsy values in python--
+# ""
+# 0
+# None
+
+print(bool(""))
